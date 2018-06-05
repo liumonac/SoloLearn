c38ce08e883d37eeb80ee18db9cbb663c391d252
liumonac <liu.mona.c@gmail.com>
2018-06-05 16:58:18 -0600

    refactor

--- NeighborNumbers/NumberFinder.java
+++ NeighborNumbers/NumberFinder.java
@@ -3,38 +3,27 @@ import java.lang.Integer;
 
 public class NumberFinder {
 	private Hashtable<String, Integer> numbers;
-	private ArrayList<String> outputList;
 
 	public NumberFinder() {
 		numbers = new Hashtable<String, Integer>();
-		outputList = new ArrayList<String>();
 	}
 
 	public String compareNumbersHash (String[] input1, String[] input2) {
+		String outputMsg = "\"";
+
 		numbers.clear();
-		outputList.clear();
+
+		input1 = formatNumbers(input1);
+		input2 = formatNumbers(input2);
 
 		for (String num : input1) {
-			//get rid of non-numeric characters
-			num = num.replaceAll("\\D+","");
 			int extractNum = Integer.parseInt (num);
 			numbers.put (num, extractNum);
 		}
-
 		for (String num : input2) {
-			//get rid of non-numeric characters
-			num = num.replaceAll("\\D+","");
-
 			if (numbers.get (num) != null) {
-				outputList.add (num);
+				outputMsg = outputMsg + num + ", ";
 			}
-		}
-
-		Collections.sort(outputList);
-
-		String outputMsg = "\"";
-		for (String num : outputList) {
-			outputMsg = outputMsg + num + ", ";
 		}
 
 		outputMsg = outputMsg.replaceAll(", $", "");
@@ -46,17 +35,8 @@ public class NumberFinder {
 	public String compareNumbers (String[] input1, String[] input2) {
 		String outputMsg = "\"";
 
-		//format numbers, get rid of non-numeric characters
-		for (int i = 0; i < input1.length; i++) {
-			input1[i] = input1[i].replaceAll("\\D+","");
-		}
-		for (int i = 0; i < input2.length; i++) {
-			input2[i] = input2[i].replaceAll("\\D+","");
-		}
-
-		//make sure output will be ascending order
-		Arrays.sort(input1);
-		Arrays.sort(input2);
+		input1 = formatNumbers(input1);
+		input2 = formatNumbers(input2);
 
 		for (String str1 : input1) {
 			for (String str2 : input2) {
@@ -70,6 +50,24 @@ public class NumberFinder {
 		outputMsg = outputMsg + "\"";
 
 		return outputMsg;
+	}
+
+	/***************************
+	 *
+	 * helper functions
+	 *
+	 ***************************/
+
+	//format numbers, get rid of non-numeric characters
+	private String[] formatNumbers (String strArr[]) {
+		for (int i = 0; i < strArr.length; i++) {
+			strArr[i] = strArr[i].replaceAll("\\D+","");
+		}
+
+		//make sure output will be ascending order
+		Arrays.sort(strArr);
+
+		return strArr;
 	}
 
 }
