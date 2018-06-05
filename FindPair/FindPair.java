ed42f0e46ce88418c772b3970d8ad667c375bfdc
liumonac <liu.mona.c@gmail.com>
2018-06-05 17:33:01 -0600

    with sorted list

--- FindPair/FindPair.java
+++ FindPair/FindPair.java
@@ -17,12 +17,45 @@ Bonus: Solve the same problem in O(n) time with an unsorted list of numbers.
 */
 
 class FindPair {
+public static boolean findSum (int[] collection, int find) {
+		boolean found = false;
+
+		int left = 0;
+		int right = collection.length - 1;
+		int sum = 0;
+
+		for (int i = 0; i < collection.length; i++) {
+			sum = collection[left] + collection[right];
+
+			if (sum == find) {
+				found = true;
+				break;
+			} else if (sum < find) {
+				left++;
+			} else if (sum > find) {
+				right--;
+			}
+
+		}
+
+		return found;
+	}
+
 	public static void main (String[] args) {
 		int[] collection = {1, 2, 4, 4};
 
 		if (args.length != 1) {
 			System.out.println ("Usage: FindPair [num]");
 		} else {
+			int find = Integer.parseInt (args[0]);
+
+			boolean found = findSum (collection, find);
+
+			if (found) {
+				System.out.println ("True");
+			} else {
+				System.out.println ("False");
+			}
 
 		}
 	}
