f3004a760c309d09033039a59bcd2c23576b488a
liumonac <liu.mona.c@gmail.com>
2018-06-05 17:25:02 -0600

    add findPair challenge

---
+++ FindPair/FindPair.java
@@ -0,0 +1,29 @@
+/*
+Prompt:
+
+You have a collection of sorted numbers, where you want to find if there 
+exists a pair whose sum is equal to the user's input.
+
+For example, for the numbers [1, 2, 4, 4]:
+
+Input: 8
+Output: True
+Input: 7
+Output: False
+
+Try to solve it in a linear time O(n)
+
+Bonus: Solve the same problem in O(n) time with an unsorted list of numbers.
+*/
+
+class FindPair {
+	public static void main (String[] args) {
+		int[] collection = {1, 2, 4, 4};
+
+		if (args.length != 1) {
+			System.out.println ("Usage: FindPair [num]");
+		} else {
+
+		}
+	}
+}
